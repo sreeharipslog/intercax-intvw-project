3f29a3de4547785e397560f686ee5adec76286f4
Sreehari P S <92331340+sreeharipslog@users.noreply.github.com>
2022-11-02 13:16:36 +0530

    Project Source, First Commit

---
+++ src/test/java/com/roboticsinc/robotinventory/RobotInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roboticsinc.robotinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class RobotInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
