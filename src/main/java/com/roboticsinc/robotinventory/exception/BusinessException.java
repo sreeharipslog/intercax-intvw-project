ccd602f24e2066f30ce70bc4e8abfec5c71087f5
Sreehari P S <92331340+sreeharipslog@users.noreply.github.com>
2022-11-03 12:52:10 +0530

    Update business exception

--- src/main/java/com/roboticsinc/robotinventory/exception/BusinessException.java
+++ src/main/java/com/roboticsinc/robotinventory/exception/BusinessException.java
@@ -1,7 +1,10 @@
 package com.roboticsinc.robotinventory.exception;
 
 /**
- * Business exception for Robot Inventory
+ * Business exception for Robot Inventory.<br>
+ * <b>Note</b>: From Spring 5, if needed we can make use of
+ * {@link org.springframework.web.server.ResponseStatusException} instead of creating multiple
+ * custom classes. One type with multiple error codes
  *
  * @author sreeharipslog
  */
@@ -11,13 +14,20 @@ public class BusinessException extends RuntimeException {
     private static final long serialVersionUID = 6596541092893697014L;
 
     private final int errorCode;
+    private final String message;
 
     public BusinessException(int errorCode, String message) {
         super(message);
         this.errorCode = errorCode;
+        this.message = message;
     }
 
     public int getErrorCode() {
         return errorCode;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
     }
 }
